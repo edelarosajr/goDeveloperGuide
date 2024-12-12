1371a2810281072f5d564eda453846ba3cce2610
Ebli De La Rosa Jr <delarosajr.e@gmail.com>
2024-12-12 15:42:59 -0600

    Removing unnecessary line

---
+++ parity/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nums []int
+	for i := 0; i <= 10; i++ {
+		nums = append(nums, i)
+	}
+
+	for _, num := range nums {
+		if num%2 == 0 {
+			fmt.Println(num, "is even")
+		} else {
+			fmt.Println(num, "is odd")
+		}
+	}
+}
