c2a5b75f88cd6f3900f6f2dcfc5ca1f5b1559591
Ebli De La Rosa Jr <delarosajr.e@gmail.com>
2024-12-16 14:25:13 -0600

    Map function

--- map/main.go
+++ map/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// creation method 3: var colors map[string]string
+
+	// creation method 2: colors := make(map[string]string)
+
+	// no dot access syntax
+
+	// delete(colors, "white")
+	// access: colors["white"] = "#FFFFFF"
+
+	// creation method 1
+	colors := map[string]string{
+		"red":   "#FF0000",
+		"green": "#00FF00",
+		"white": "#FFFFFF",
+	}
+
+	printMap(colors)
+
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, "hex value is", hex)
+	}
 }
